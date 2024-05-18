7239dfeb9b05314f6bf0af6ecf494a4046788aa9
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-18 14:26:26 +0100

    Task1 : q1,2,3,4,5,6,7 complete

---
+++ LAB 2/logging/src/main/java/com/example/logging/MyApplicationRunner.java
@@ -0,0 +1,25 @@
+package com.example.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyApplicationRunner implements CommandLineRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyApplicationRunner.class) ;
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOGGER.info("Application started");
+
+        try{
+             int x = 3 / 0 ;
+        }catch(Exception e){
+           LOGGER.error("Errur occurred: "+ e.getMessage());
+        }
+        LOGGER.info("Application finished");
+    }
+
+}
