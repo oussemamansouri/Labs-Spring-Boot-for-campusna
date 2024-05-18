e0f136d5f68893636bc0cbeece0bb3f51ac6b143
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-18 13:41:36 +0100

    lab 1 complete

---
+++ LAB 1/firstspringbootproject/src/main/java/com/example/firstspringbootproject/FirstspringbootprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringbootprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspringbootprojectApplication.class, args);
+	}
+
+}
---
+++ LAB 1/secondspringbootproject/src/main/java/com/example/secondspringbootproject/SecondspringbootprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondspringbootprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondspringbootprojectApplication.class, args);
+	}
+
+}
