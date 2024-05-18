938561b219f71b944bb4ebd5c0daa7bc0860282a
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-18 14:03:56 +0100

    Re-create the lab 2 project

---
+++ LAB 2/logging/src/test/java/com/example/logging/LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
