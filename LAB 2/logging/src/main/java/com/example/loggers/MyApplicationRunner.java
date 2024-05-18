7ca129d17de3beda097a439598842c25fd72547f
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-18 14:58:02 +0100

    task 1 , q8,9,10,11 complete

--- LAB 2/logging/src/main/java/com/example/loggers/MyApplicationRunner.java
+++ LAB 2/logging/src/main/java/com/example/loggers/MyApplicationRunner.java
@@ -1,4 +1,4 @@
-package com.example.logging;
+package com.example.loggers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- LAB 2/logging/src/main/java/com/example/logging/LoggingApplication.java
+++ LAB 2/logging/src/main/java/com/example/logging/LoggingApplication.java
@@ -2,8 +2,10 @@ package com.example.logging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.example.logging","com.example.loggers"})
 public class LoggingApplication {
 
 	public static void main(String[] args) {
