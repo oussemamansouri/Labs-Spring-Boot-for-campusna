9118bb8fc4ff74871cf749ff6ae55fd78930d942
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-18 13:59:01 +0100

    create the logging project for lab 2

---
+++ LAB 2/loggingproject/src/test/java/com/elife/loggingproject/LoggingprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elife.loggingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
