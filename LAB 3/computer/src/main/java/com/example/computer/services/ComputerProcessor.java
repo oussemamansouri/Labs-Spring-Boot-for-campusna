8a2bb53716a9ee750ea507c42e32848e9e6ac29f
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-18 19:23:09 +0100

    Q1,2 complete ( add class CompetProssor and MyRunner)

--- LAB 3/computer/src/main/java/com/example/computer/services/ComputerProcessor.java
+++ LAB 3/computer/src/main/java/com/example/computer/services/ComputerProcessor.java
@@ -1,5 +1,36 @@
 package com.example.computer.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.example.computer.models.Doubler;
+import com.example.computer.models.Squarer;
+
 public class ComputerProcessor {
+
+    private List <Object>  computers = new ArrayList<Object>();
+
+    public ComputerProcessor addComputer(Object o){
+        computers.add(o);
+        return ComputerProcessor.this ;
+    }
+
+    public void computeAll(long value){
+        for (Object o : computers){
+            long computerValue = -1 ;
+
+            if( o instanceof Doubler){
+                computerValue = ((Doubler) o).computeDouble(value); 
+            }else if(o instanceof Squarer){
+                computerValue = ((Squarer) o).computeSquare(value);
+            }
+
+            String name = o.getClass().getSimpleName();
+            System.out.println("Computer: " + name + ", value:  "+ value +
+            " computed value: " + computerValue);
+
+        }
+    }
     
 }
--- LAB 3/computer/src/main/java/com/example/computer/services/MyRunner.java
+++ LAB 3/computer/src/main/java/com/example/computer/services/MyRunner.java
@@ -1,13 +1,22 @@
 package com.example.computer.services;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+import com.example.computer.models.Doubler;
+import com.example.computer.models.Squarer;
+
+@Component
 public class MyRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        new ComputerProcessor()
+        .addComputer(new Doubler())
+        .addComputer(new Squarer())
+        .computeAll(8);
        
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
     }
     
 }
