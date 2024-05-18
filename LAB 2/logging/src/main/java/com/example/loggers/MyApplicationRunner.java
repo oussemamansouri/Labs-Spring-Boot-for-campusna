0e9434eb85c5405e025697403d37b47fe3cfff0a
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-18 16:15:55 +0100

    Task 2 [OPTIONAL] complete

--- LAB 2/logging/src/main/java/com/example/loggers/MyApplicationRunner.java
+++ LAB 2/logging/src/main/java/com/example/loggers/MyApplicationRunner.java
@@ -1,5 +1,9 @@
 package com.example.loggers;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -13,13 +17,40 @@ public class MyApplicationRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         LOGGER.info("Application started");
-
+        copyLogFile();
+        
         try{
              int x = 3 / 0 ;
         }catch(Exception e){
            LOGGER.error("Errur occurred: "+ e.getMessage());
         }
         LOGGER.info("Application finished");
+    }
+
+    public void copyLogFile(){
+        File sourceFile = new File("logs/mylog.log");
+        File destinationFile = new File("logs/old/mylogs.log");
+
+        try{
+            if (!sourceFile.exists()){
+                sourceFile.getParentFile().mkdirs();
+                sourceFile.createNewFile();
+            }
+
+            if (!destinationFile.exists()){
+                destinationFile.getParentFile().mkdirs();
+                destinationFile.createNewFile();
+            }
+
+            Files.copy(sourceFile.toPath(), destinationFile.toPath(), java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+
+            LOGGER.info("Log file has been successfully copied to " + destinationFile.getAbsolutePath());
+
+        }catch(IOException e){
+
+            LOGGER.error("Errur copying log file"+ e.getMessage());
+
+        }
     }
 
 }
