7cb27bd9914939bb0ec2f11c5b99d10f561dfc9f
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-22 14:04:02 +0100

    Create the lab 4 project and add the product model
    create the lab 4 project and add the product model

---
+++ LAB 4/onlinestore/src/main/java/com/example/onlinestore/web/Models/Product.java
@@ -0,0 +1,19 @@
+package com.example.onlinestore.web.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    private Long id ;
+    private String code ;
+    private String name ;
+    private Double String ;
+    private int quantity ;
+    private String image ;
+
+}
