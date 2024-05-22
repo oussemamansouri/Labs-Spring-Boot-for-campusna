433efe83d3f79344ead54f6bd0993f8fbe80ca07
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-22 17:23:17 +0100

    Add LAB 4 plus for more practice

---
+++ LAB 4 PLUS/onlinestore/src/main/java/com/example/onlinestore/OnlinestoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinestoreApplication.class, args);
+	}
+
+}
