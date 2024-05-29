77a79f73d8e21bd62ba431805f142608f862d3d6
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-29 01:30:26 +0100

    update index.html

---
+++ LAB 5 with download image/onlinestore/src/main/java/com/example/onlinestore/web/Models/requests/ProductForm.java
@@ -0,0 +1,38 @@
+package com.example.onlinestore.web.Models.requests;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductForm {
+
+    @NotBlank(message = "Code is required")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Code must be alphanumeric")
+    private String code;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    private String name;
+
+    @NotNull(message = "Price is required")
+    @Min(value = 1, message = "Price must be a positive number")
+    private Double price;
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 1, message = "Quantity must be a positive number")
+    private Integer quantity;
+
+    private String image ;
+
+}
