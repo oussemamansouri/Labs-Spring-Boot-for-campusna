3860a13faea052a42d8b9c25ac0372b71dbd507f
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-31 23:59:55 +0100

    Update ProductRepository.java

--- LAB 5 - with Upload images and  using database/onlinestore/src/main/java/com/example/onlinestore/dao/repositories/ProductRepository.java
+++ LAB 5 - with Upload images and  using database/onlinestore/src/main/java/com/example/onlinestore/dao/repositories/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.example.onlinestore.dao.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.onlinestore.dao.entities.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
 }
