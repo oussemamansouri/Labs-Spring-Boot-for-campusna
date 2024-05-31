16b025309c3017be90d5be4c61053b707c306f7a
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-31 23:29:03 +0100

    Update ProductForm.java

--- LAB 5 - with Upload images and  using database/onlinestore/src/main/java/com/example/onlinestore/web/Models/requests/ProductForm.java
+++ LAB 5 - with Upload images and  using database/onlinestore/src/main/java/com/example/onlinestore/web/Models/requests/ProductForm.java
@@ -3,7 +3,6 @@ package com.example.onlinestore.web.Models.requests;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
