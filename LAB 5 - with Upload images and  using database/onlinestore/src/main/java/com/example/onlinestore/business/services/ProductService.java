9588deca831ad0b50e6550751e0f87cc6416dbd1
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-31 22:45:51 +0100

    Create ProductService.java

---
+++ LAB 5 - with Upload images and  using database/onlinestore/src/main/java/com/example/onlinestore/business/services/ProductService.java
@@ -0,0 +1,22 @@
+package com.example.onlinestore.business.services;
+
+import java.util.List;
+
+import com.example.onlinestore.dao.entities.Product;
+
+public interface ProductService {
+
+    //read operation
+    List<Product> getAllProduct();
+    Product getProductById();
+
+    //create operation
+    Product addProduct(Product product);
+
+    //update operation
+    Product updateProduct(Product product, Long id);
+
+    //delete operation
+    void deleteProduct(Long id);
+
+}
