54bd0c3f63cb2674b52eef2b7a5bfd49be0ba1c9
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-31 23:29:06 +0100

    Create ProductServiceImp.java

---
+++ LAB 5 - with Upload images and  using database/onlinestore/src/main/java/com/example/onlinestore/business/servicesImp/ProductServiceImp.java
@@ -0,0 +1,47 @@
+package com.example.onlinestore.business.servicesImp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.onlinestore.business.services.ProductService;
+import com.example.onlinestore.dao.entities.Product;
+import com.example.onlinestore.dao.repositories.ProductRepository;
+
+public class ProductServiceImp implements ProductService {
+
+    @Autowired
+    ProductRepository productRepository ;
+
+    //OR
+    // final ProductRepository productRepository ;
+    // public ProductServiceImp(ProductRepository productRepository ){
+    //     this.productRepository= productRepository;
+    // }
+
+    @Override
+    public List<Product> getAllProduct() {
+        return this.productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById( Long id) {
+        return this.productRepository.findById(id).get();
+    }
+
+    @Override
+    public Product addProduct(Product product) {
+       return this.productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(Product product) {
+       return this.productRepository.save(product);
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+      this.productRepository.deleteById(id);
+    }
+
+}
