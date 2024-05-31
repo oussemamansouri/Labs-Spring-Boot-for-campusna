f3f1cf542a8d594bb9c982e57498af6bcf82e91a
oussemamansouri <oussemamansouri619@gmail.com>
2024-05-31 23:59:58 +0100

    Update ProductServiceImp.java

--- LAB 5 - with Upload images and  using database/onlinestore/src/main/java/com/example/onlinestore/business/servicesImp/ProductServiceImp.java
+++ LAB 5 - with Upload images and  using database/onlinestore/src/main/java/com/example/onlinestore/business/servicesImp/ProductServiceImp.java
@@ -3,11 +3,13 @@ package com.example.onlinestore.business.servicesImp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.onlinestore.business.services.ProductService;
 import com.example.onlinestore.dao.entities.Product;
 import com.example.onlinestore.dao.repositories.ProductRepository;
 
+@Service
 public class ProductServiceImp implements ProductService {
 
     @Autowired
